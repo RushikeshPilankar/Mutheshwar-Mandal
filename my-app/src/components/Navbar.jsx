e7506a2bc5cf5a88e161a581cdd02901e094d86e
rushi <rushikeshsp@gmail.com>
2026-07-19 02:14:14 +0530

    logo rushikesh.pilankar

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom";
-
+import logo from "../assets/m.jpg";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">Mutheshwar Mandal</div>
+      <div className="navbar-brand">
+        <img src={logo} alt="Mutheshwar Mandal Logo" className="navbar-logo" />
+        <span>Mutheshwar Mandal</span>
+      </div>
       <div className="navbar-links">
         <NavLink to="/" end className={({ isActive }) => (isActive ? "active" : "")}>
           Dashboard
